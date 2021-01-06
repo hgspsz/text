aa2f1ef1e47bb30de45678d37f50838fcebfe600
Gergely Nagy <nagy.gergely14@gmail.com>
2021-01-06 19:46:46 +0100

    Test issue with Workflow

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 rm -rf target
 mkdir target
 cd target
-git pull https://github.com/hgspsz/theme
+git clone https://github.com/hgspsz/theme .
 
-cp -r ../archectypes ./archectypes
+cp -r ../archetypes ./archetypes
 cp -r ../content ./content
 cp -r ../static ./static
 
-docker run --rm -it -v $PWD:/src -p 1313:1313 -u hugo jguyomard/hugo-builder hugo -D
+docker run --rm -it -v $PWD:/src -p 1313:1313 -u hugo jguyomard/hugo-builder hugo
